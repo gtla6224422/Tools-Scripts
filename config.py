4f38a99e995736269f1316a759d3d05048dfb61a
gtla6224422 <nonop01@163.com>
2025-02-06 14:56:31 +0800

    改下日志文件目录

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 # config.py
+import os
 
 class Config:
     DATABASE_CONFIG = {
@@ -9,4 +10,10 @@ class Config:
         'charset': 'utf8mb4',  # 根据需要设置字符集
     }
 
-    OUTPUT_DIR = 'D:/testfile/'  # 指定输出路径
+
+    # 使用相对路径指向项目根目录下的outputfile文件夹
+    OUTPUT_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'outputfile')
+    
+    # 确保目录存在
+    if not os.path.exists(OUTPUT_DIR):
+        os.makedirs(OUTPUT_DIR)
